15959d0f623cc46cbc78adaaeca704f0db2994e9
andisiambuku <roselandambuku@gmail.com>
2022-08-21 12:44:43 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+// const drivers =  ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+function findMatching(drivers,name){
+    return drivers.filter( input=>(
+      input.toLowerCase() === name.toLowerCase()
+    ));
+}
+
+function fuzzyMatch(drivers,name){
+return drivers.filter(input =>(
+  input.slice(0,name.length)=== name
+));
+}
+
+function matchName(drivers,name){
+  return drivers.filter(input =>(
+    input.name === name
+  ));
+}
